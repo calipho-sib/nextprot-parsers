d1d030cd69e9ab5696cee84138bb36e9b86cd7a4
agateau <alain.gateau@isb-sib.ch>
2014-02-05 15:30:03 +0100

    missing classes

---
+++ src/main/scala/org/nextprot/parser/core/datamodel/antibody/AntibodyIdentifierProperty.scala
@@ -0,0 +1,17 @@
+package org.nextprot.parser.core.datamodel.antibody
+
+class AntibodyIdentifierProperty(val _propertyname: String, val _propertyvalue: String) {
+
+  def toXML =
+        <com.genebio.nextprot.datamodel.identifier.IdentifierProperty>
+          <cvPropertyName>
+            <cvName> { _propertyname } </cvName>
+          </cvPropertyName>
+          <propertyValue>{scala.xml.PCData(_propertyvalue)}</propertyValue>
+          <sequenceIdentifier class="com.genebio.nextprot.datamodel.identifier.AntibodyIdentifier" reference="../../.."/>
+        </com.genebio.nextprot.datamodel.identifier.IdentifierProperty>
+}
+
+class AntibodyIdentifierPropertyList(val _antibodyIdentifierPropertyList: List[AntibodyIdentifierProperty]){
+def toXML = <identifierProperties> { _antibodyIdentifierPropertyList.map(_.toXML) } </identifierProperties>;
+}
---
+++ src/main/scala/org/nextprot/parser/core/datamodel/biosequence/BioSequence.scala
@@ -0,0 +1,24 @@
+package org.nextprot.parser.core.datamodel.biosequence
+
+class BioSequenceList(val _sequences: List[BioSequence]) {
+  
+  
+  def toXML = 
+      if (_sequences(0)._sequence != "") {
+      <bioSequences>
+  		{_sequences.map(_.toXML)}
+      </bioSequences> }
+      //else null
+    
+}
+
+class BioSequence (val _sequence: String, val _sequenceType: String) {
+  
+  def toXML =
+        <com.genebio.nextprot.datamodel.identifier.BioSequence>
+          <bioSequence>{_sequence}</bioSequence>
+          <bioSequenceType>{_sequenceType}</bioSequenceType>
+          <sequenceIdentifier class="com.genebio.nextprot.datamodel.identifier.AntibodyIdentifier" reference="../../.."/>
+        </com.genebio.nextprot.datamodel.identifier.BioSequence>
+    
+}
