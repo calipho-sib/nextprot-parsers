ffc04323e3ed9fe8aee206dd4ffc9010fbccbb74
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2014-02-10 08:57:31 +0100

    generalisation of RawAnnotation and AnnotationResourceAssoc, new templates for expression annotations

--- src/main/scala/org/nextprot/parser/core/datamodel/annotation/AnnotationResourceAssoc.scala
+++ src/main/scala/org/nextprot/parser/core/datamodel/annotation/AnnotationResourceAssoc.scala
@@ -2,7 +2,11 @@ package org.nextprot.parser.core.datamodel.annotation
 
 import org.nextprot.parser.core.constants.NXQuality.NXQuality
 
-class AnnotationResourceAssoc(val _resourceClass: String, val _resourceType: String, val _accession: String, val _cvDatabaseName: String, val _qualifierType: String, val _isNegative: Boolean, val _type: String, val _quality: NXQuality, val _dataSource: String) {
+class AnnotationResourceAssoc(
+    val _resourceClass: String, val _resourceType: String, val _accession: String, 
+    val _cvDatabaseName: String, val _qualifierType: String, val _isNegative: Boolean, 
+    val _type: String, val _quality: NXQuality, val _dataSource: String,
+    val _props: List[AnnotationResourceAssocProperty], val _expContext: ExperimentalContextSynonym) {
   def toXML =
     <com.genebio.nextprot.datamodel.annotation.AnnotationResourceAssoc>
       <resource class={ _resourceClass }>
@@ -13,8 +17,22 @@ class AnnotationResourceAssoc(val _resourceClass: String, val _resourceType: Str
         </cvDatabase>
       </resource>
       <qualifierType>{ _qualifierType }</qualifierType>
-      {if (_quality != null)
-      {<qualityQualifier>{ _quality.toString() }</qualityQualifier>}
+      {
+        if (_quality != null) {<qualityQualifier>{ _quality.toString() }</qualityQualifier>}
+      }
+      {
+        if (_props != null && ! _props.isEmpty) {
+          <annotationResourceAssocProperties>{ _props.map(_.toXML) } </annotationResourceAssocProperties>
+        }
+      }
+      {
+        if ( _expContext != null) {
+      	  <experimentalContext>
+            <contextSynonyms>
+        		{ _expContext.toXML }
+            </contextSynonyms>
+          </experimentalContext>          
+        }
       }
       <isNegativeEvidence>{ _isNegative }</isNegativeEvidence>
       <type>{ _type }</type>
