4fa86a53d0f2d5891b5e23595938962b48e02d21
Daniel <ddtxra@gmail.com>
2014-02-17 17:30:14 +0100

    adding a pretty reducer to print pretty if necessary

--- src/main/scala/org/nextprot/parser/core/impl/NXSimpleFileReducer.scala
+++ src/main/scala/org/nextprot/parser/core/impl/NXSimpleFileReducer.scala
@@ -9,22 +9,14 @@ import org.nextprot.parser.core.stats.StatisticsCollector
 import org.nextprot.parser.core.stats.StatisticsCollectorSingleton
 import org.nextprot.parser.core.NXProperties
 
-class NXSimpleFileReducer extends NXReducer {
+class NXSimpleFileReducer extends NXPrettyReducer {
 
-  val prettyFormat = (System.getProperty(NXProperties.prettyPrint) != null)
-  
-  if(prettyFormat)
-    println("Warning! Using pretty print.... This configuration is n ot performant and will take some time... (useful for debug)");
-  else   println("Not using pretty print (much more performant)! If you want to use pretty print use the system property " + NXProperties.prettyPrint);
-  
-  val prettyPrinter = new PrettyPrinter(1000, 4);
   val fw = new FileWriter(System.getProperty("output.file"), false)
 
   def reduce(objects: Any) = {
     objects match {
       case tm: TemplateModel => {
-        val text = if(prettyFormat) prettyPrinter.format(tm.toXML) else tm.toXML;
-        fw.write(text + "\n")
+        fw.write(getPrettyFormatIfNeeded(tm.toXML) + "\n")
         StatisticsCollectorSingleton.increment("ENTRIES-QUALITY", tm.getQuality.toString())
       }
       case _ => throw new ClassCastException
