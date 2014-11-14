03811498c4ebee3068b7b223e74d960243a580a2
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2014-11-14 11:11:39 +0100

    added comments about implicit cases now explicitly validated by paula, no functional change

--- hpa/src/main/scala/org/nextprot/parser/hpa/subcell/rules/APEQualityRule.scala
+++ hpa/src/main/scala/org/nextprot/parser/hpa/subcell/rules/APEQualityRule.scala
@@ -10,22 +10,48 @@ import org.nextprot.parser.hpa.subcell.constants.HPAAPEReliabilityValue._
 
 case class APEQualityRule(reliability: HPAAPEReliabilityValue, hpaPA: HPAAPEValidationValue, hpaWB: HPAAPEValidationValue) {
 
+/* 
+ * Specs quality rules for tissue expression / subcell.location - table for APE (and other cases since 11.2014), document version 12
+ * 
+ * HPA Reliability		HPA PA				       		WB						        	 NP
+ * 
+ 1 Supportive		   supportive at least one AB	supportive at least one AB				Gold
+ 2 Supportive		   supportive at least one AB	uncertain all AB						Silver
+ 3 Supportive		   supportive at least one AB	non-supportive all AB					Silver
+ 4 Supportive		   supportive at least one AB	uncertain and non-supportive ABs		Silver
+ 5 Supportive		   uncertain all AB			-											Silver
+ 
+HPA Reliability			HPA PA			        		WB						         	 NP
+
+ 6 Uncertain		   supportive all AB		    supportive all AB				    	Silver
+ 7 Uncertain		   supportive all AB		    uncert./non-supp. at least one AB		Silver
+ 8 Uncertain		   uncertain at least one AB	supportive all AB						Silver
+ 9 Uncertain		   uncertain at least one AB	uncert./non-supp. at least one AB		Bronze
+ 
+HPA Reliability			HPA PA				           	WB									 NP
+
+10 Non-supportive      supportive all AB		    supportive all AB						Bronze
+11 Non-supportive	   supportive all AB		    uncert./non-supp. at least one AB		Bronze
+12 Non-supportive	   uncertain at least one AB	supportive all AB						Bronze
+13 Non-supportive	   uncertain at least one AB	uncert./non-supp. at least one AB		Bronze
+ * 
+ */
   /** 
-   *  To be validated with Paula
+   *  Validated with Paula 14.11.2014
    */
   def getQuality: NXQuality = {
     val res: NXQuality =
      APEQualityRule.this match {
-      case APEQualityRule(Not_supportive, _, _) => BRONZE
-      case APEQualityRule(Uncertain, Not_Supportive, _) => BRONZE              // check with Paula
-      case APEQualityRule(Uncertain, UncertainOne, Not_Supportive) => BRONZE
-      case APEQualityRule(Uncertain, UncertainOne, UncertainOne) => BRONZE
-      case APEQualityRule(Uncertain, UncertainOne, UncertainAll) => BRONZE     // check with Paula
-      case APEQualityRule(Uncertain, UncertainOne, SupportiveOne) => BRONZE
-      case APEQualityRule(Supportive, SupportiveOne, SupportiveOne) => GOLD
-      case APEQualityRule(Supportive, SupportiveOne, SupportiveAll) => GOLD
-      case APEQualityRule(Supportive, SupportiveAll, SupportiveOne) => GOLD
-      case APEQualityRule(Supportive, SupportiveAll, SupportiveAll) => GOLD
+      case APEQualityRule(Not_supportive, _, _) => BRONZE  						// case 10,11,12,13
+      case APEQualityRule(Uncertain, Not_Supportive, _) => BRONZE               // case unspecified in specs but approved by Paula, mail 14.11.2014
+      case APEQualityRule(Uncertain, UncertainOne, Not_Supportive) => BRONZE	// case 9 with WB=non supp.
+      case APEQualityRule(Uncertain, UncertainOne, UncertainOne) => BRONZE		// case 9 with WB uncertain one
+      case APEQualityRule(Uncertain, UncertainOne, UncertainAll) => BRONZE      // case unspecified in specs but approved by Paula, mail 14.11.2014
+      case APEQualityRule(Uncertain, UncertainOne, SupportiveOne) => BRONZE		// case 9
+      case APEQualityRule(Supportive, SupportiveOne, SupportiveOne) => GOLD     // case 1
+      case APEQualityRule(Supportive, SupportiveOne, SupportiveAll) => GOLD     // case 1
+      case APEQualityRule(Supportive, SupportiveAll, SupportiveOne) => GOLD     // case 1
+      case APEQualityRule(Supportive, SupportiveAll, SupportiveAll) => GOLD     // case 1
       case _ => SILVER
     }
     return res
@@ -68,5 +94,6 @@ case class APEQualityRule(reliability: HPAAPEReliabilityValue, hpaPA: HPAAPEVali
       case _ => throw new Exception("APEQualityRule not found: " + this)
     }
 
-
+  
+  
 }
