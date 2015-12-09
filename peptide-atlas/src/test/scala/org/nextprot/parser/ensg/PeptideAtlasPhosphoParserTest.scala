77fde97bf97347be64a171c9736b335fb655ea1e
agateau <alain.gateau@gmail.com>
2015-12-09 17:46:15 +0100

    first commit

--- peptide-atlas/src/test/scala/org/nextprot/parser/ensg/PeptideAtlasPhosphoParserTest.scala
+++ peptide-atlas/src/test/scala/org/nextprot/parser/ensg/PeptideAtlasPhosphoParserTest.scala
@@ -1,18 +1,52 @@
 package org.nextprot.parser.peptide.atlas
 
 import org.scalatest.FlatSpec
+import org.nextprot.parser.peptide.atlas.datamodel.Peptide
+import org.nextprot.parser.peptide.atlas.datamodel.Feature
+import org.nextprot.parser.peptide.atlas.datamodel.DbXref
 import org.scalatest.Matchers
 
 class PeptideAtlasPhosphoParserTest extends FlatSpec with Matchers {
 
+    val dbref1 = new DbXref(_mData="MDATA_0066", _pmid="24400987", _quality=null)
+    val dbref2 = new DbXref(_mData="MDATA_0070", _pmid=null, _quality=null)
+    val dbref3 = new DbXref(_mData="MDATA_0066", _pmid="24400987", _quality="GOLD")
+    val dbref4 = new DbXref(_mData="MDATA_0070", _pmid=null, _quality="SILVER")
+    val dbref5 = new DbXref(_mData="MDATA_0101", _pmid="18272233", _quality="SILVER")
+    val feature1 = new Feature(_position=7, _description="phosphoserine", _dbrefs=List(dbref3,dbref4))
+    val feature2 = new Feature(_position=20, _description="phosphoserine", _dbrefs=List(dbref4))
+
+  
+  
   it should "extract all peptides from a sample" in {
 
     val parser = new PeptideAtlasPhosphoNXParser()
 
-    //parser.parse(getClass.getResource("sample.tsv").getFile)
+    parser.parse("/home/agateau/workspace/nextprot-parsers/peptide-atlas/src/test/resources/org/nextprot/parser/peptide/atlas/peptide_ptm.tsv");
+    //parser.parse("/home/agateau/workspace/nextprot-parsers/peptide-atlas/src/test/resources/org/nextprot/parser/peptide/atlas/sample1.tsv");
+    
 
     /*assert("A0AVT1" == accession)
     assert("ENSG00000033178,ENSG00000033178" == ensgs)*/
   }
 
+  it should "output proper DbXref XML" in {
+
+    val dbref = new DbXref(_mData="MDATA_0066", _pmid="24400987", _quality=null)
+
+    println(dbref.toXML)
+  }
+  
+  it should "output proper Feature XML" in {
+
+    println(feature1.toXML)
+  }
+  
+
+  it should "output proper Peptide XML" in {
+
+    val peptide = new Peptide(_sequence="RPGGEPSPEGTTGQSYNQYSQR", id="PAp00000083", _dbrefs=List(dbref1,dbref2), _features=List(feature1,feature2))
+
+    println(peptide.toXML)
+  }
 }
