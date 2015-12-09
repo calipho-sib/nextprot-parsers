35511a086deb668d526842bc618f48403b79e8e4
agateau <alain.gateau@gmail.com>
2015-12-09 17:45:20 +0100

    first commit

--- peptide-atlas/src/main/scala/org/nextprot/parser/peptide/atlas/datamodel/Feature.scala
+++ peptide-atlas/src/main/scala/org/nextprot/parser/peptide/atlas/datamodel/Feature.scala
@@ -1,10 +1,21 @@
 package org.nextprot.parser.peptide.atlas.datamodel
 
-class Feature(val _position: Integer) {
+import org.nextprot.parser.core.constants.NXQuality.NXQuality
+
+class Feature(val _position: Integer, val _description: String, val _dbrefs: List[DbXref]) {
   
   def toXML = 
-    <feature>
-      <position>{_position}</position>
-    </feature>
+    <feature type="modified residue" description={_description} quality={getTopQuality}>
+			<location>
+				<position position={_position.toString()} />
+			</location> 
+			<evidence type="mass spectrometry evidence" assigned_by="PeptideAtlas human phosphoproteome"/>
+    {_dbrefs.map(_.toXML)}
+		</feature>
   
+     def getTopQuality: String = {
+    _dbrefs.foreach { dbref => if(dbref._quality == "GOLD") return("GOLD")} 
+    "SILVER"
+    }
 }
+
