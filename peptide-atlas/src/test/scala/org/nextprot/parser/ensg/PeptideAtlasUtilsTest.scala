492be5fb4b5bf8f302cdd33c6e812cc2309d02f0
agateau <alain.gateau@gmail.com>
2015-12-09 17:46:29 +0100

    first commit

--- peptide-atlas/src/test/scala/org/nextprot/parser/ensg/PeptideAtlasUtilsTest.scala
+++ peptide-atlas/src/test/scala/org/nextprot/parser/ensg/PeptideAtlasUtilsTest.scala
@@ -5,4 +5,9 @@ import org.scalatest.Matchers
 
 class PeptideAtlasUtilsTest extends FlatSpec with Matchers {
 
+  it should "return proper mod_res" in {
+
+    val modseq = "n[145]RLS[167]EDYGVLK[272]"
+    assert("phosphoserine" == PeptideAtlasUtils.getMod(modseq, 3))
+  }
 }
