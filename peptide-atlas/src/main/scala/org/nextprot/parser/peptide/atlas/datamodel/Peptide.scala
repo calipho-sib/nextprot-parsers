c718bf307823bf9f21d14b02a064f7341a97d412
agateau <alain.gateau@gmail.com>
2015-12-09 17:45:32 +0100

    first commit

--- peptide-atlas/src/main/scala/org/nextprot/parser/peptide/atlas/datamodel/Peptide.scala
+++ peptide-atlas/src/main/scala/org/nextprot/parser/peptide/atlas/datamodel/Peptide.scala
@@ -2,15 +2,19 @@ package org.nextprot.parser.peptide.atlas.datamodel
 
 import org.nextprot.parser.core.constants.NXQuality.NXQuality
 import org.nextprot.parser.core.datamodel.TemplateModel
-
-class Peptide(val _quality: NXQuality, val _features: List[Feature]) extends TemplateModel {
+import scala.collection.mutable.ListBuffer
+class Peptide(val _sequence: String, val id: String, val _dbrefs: List[DbXref], val _features: List[Feature]) extends TemplateModel {
+  
   override def toXML =
-    <peptide>
-      <dbReference></dbReference>
+    <peptide sequence={_sequence} >
+    <evidence type="mass spectrometry evidence" assigned_by="PeptideAtlas human phosphoproteome"/>
+    <dbReference type="PeptideAtlas" id={id}></dbReference>
+    {_dbrefs.map(_.toXML)}
       {
-        _features.map(_.toXML)
+        if(_features != null)_features.map(_.toXML)
       }
     </peptide>
 
-  override def getQuality: NXQuality = _quality;
+   override def getQuality: NXQuality = org.nextprot.parser.core.constants.NXQuality.GOLD;
+   
 }
