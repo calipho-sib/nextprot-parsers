c8fb214f272b4c31ec81266d596f32446a5a4e04
agateau <alain.gateau@gmail.com>
2015-12-14 13:12:11 +0100

    progress

--- peptide-atlas/src/test/scala/org/nextprot/parser/ensg/PeptideAtlasUtilsTest.scala
+++ peptide-atlas/src/test/scala/org/nextprot/parser/ensg/PeptideAtlasUtilsTest.scala
@@ -2,12 +2,30 @@ package org.nextprot.parser.peptide.atlas
 
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
+import scala.collection.mutable.ArrayBuffer
 
 class PeptideAtlasUtilsTest extends FlatSpec with Matchers {
+
+    val peplines = ArrayBuffer("RPGGEPSPEGTTGQSYNQYSQR  RPGGEPSPEGTTGQSYNQYS[167]QR 20  1.000 5304- P02751",
+        "RPGGEPSPEGTTGQSYNQYSQR RPGGEPSPEGTTGQSYNQYS[167]QR 20  0.993 5305- P02751",
+        "RPGGEPSPEGTTGQSYNQYSQR RPGGEPS[167]PEGTTGQSYNQYSQR 7 1.000 5305- P02751")
 
   it should "return proper mod_res" in {
 
     val modseq = "n[145]RLS[167]EDYGVLK[272]"
     assert("phosphoserine" == PeptideAtlasUtils.getMod(modseq, 3))
   }
+
+  it should "return proper modified positions count" in {
+
+    val poslist = PeptideAtlasUtils.getDistinctModPos(peplines)
+    assert(2 == poslist.size)
+  }
+  
+  it should "return proper modified modification" in {
+
+    val mod = PeptideAtlasUtils.getModresForFeature(peplines,20)
+    assert("phosphoserine" == mod)
+  }
+  
 }
