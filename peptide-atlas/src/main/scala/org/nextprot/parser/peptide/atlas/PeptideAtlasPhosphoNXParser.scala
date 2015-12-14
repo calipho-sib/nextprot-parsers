f0251ec8a303524942773d97f9c27af4575cbfb8
agateau <alain.gateau@gmail.com>
2015-12-14 13:10:31 +0100

    progress

--- peptide-atlas/src/main/scala/org/nextprot/parser/peptide/atlas/PeptideAtlasPhosphoNXParser.scala
+++ peptide-atlas/src/main/scala/org/nextprot/parser/peptide/atlas/PeptideAtlasPhosphoNXParser.scala
@@ -6,41 +6,41 @@ import scala.io.Source
 import org.nextprot.parser.peptide.atlas.datamodel.Peptide
 import org.nextprot.parser.peptide.atlas.datamodel.DbXref
 import org.nextprot.parser.peptide.atlas.datamodel.Feature
-import scala.collection.concurrent.TrieMap
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.HashMap
 
 class PeptideAtlasPhosphoNXParser extends App {
 
   var pInfo = ""
-  //val topHeader = "<entries version="201509" datarelease="PeptideAtlas human phosphoproteome" datasource="PeptideAtlas human phosphoproteome">"
-  val printer = new scala.xml.PrettyPrinter(80, 2)
-
+  var pep_count :Int = 0
   def parsingInfo: String = pInfo
   
+  //val pepsMap: HashMap[String, ArrayBuffer[String]] = new HashMap();   
+  //val pepsMap: HashMap[String, ArrayBuffer[String]]   
+  
   def parse(filename: String) = {
-    // First parse the tsv file of sampleId-MDATA-pmid association and guild a map with sampleIds as keys
-  val sampleIdMap: HashMap[String, String] = new HashMap(); 
-  var mData = ""
-  val src1 = Source.fromFile("/home/agateau/workspace/nextprot-parsers/peptide-atlas/src/test/resources/org/nextprot/parser/peptide/atlas/Metadata_phosphosetPA.tsv")  
-    for(line <- src1.getLines().drop(1)) {
-      val lineTokens = line.split("\\s+")
-      if(lineTokens(0).startsWith("MDATA")) mData = lineTokens(0) + "-" + lineTokens(2) // group MDATA id and pmid
-      sampleIdMap(lineTokens(1)) = mData
-    }
-  
-  // Now parse the data file
-  val pepsMap: TrieMap[String, ArrayBuffer[String]] = new TrieMap();    
-  val src = Source.fromFile(filename)  
-  
+  val pprinter=new scala.xml.PrettyPrinter(240, 2)
+  val topHeader = "<entries version=\"201509\" datarelease=\"PeptideAtlas human phosphoproteome\" datasource=\"PeptideAtlas human phosphoproteome\">"
+  // First parse the tsv file of sampleId-MDATA-pmid association and guild a map with sampleIds as keys
+  //val sampleIdMap: HashMap[String, String] = PeptideAtlasUtils.getMetadataMap1("/home/agateau/workspace/nextprot-parsers/peptide-atlas/src/test/resources/org/nextprot/parser/peptide/atlas/Metadata_phosphosetPA.tsv"); 
+  val sampleIdMap: HashMap[String, String] = PeptideAtlasUtils.getMetadataMap("/share/sib/common/Calipho/np/metadata_files/metadata.txt"); 
+
+  // Now parse the data file and build a map with PaPids as key
+   val pepsMap: HashMap[String, ArrayBuffer[String]] = new HashMap();    
+   val src = Source.fromFile(filename)  
+  //pepsMap = new HashMap()
     for(line <- src.getLines().drop(1)) {
       val key = line.substring(0,11)
       val pepRows = pepsMap.getOrElse(key, new ArrayBuffer());
       pepRows.append(line.substring(12));
       pepsMap.put(key, pepRows);
     }
-
-  for(pep <- pepsMap) {
+   src.close() 
+   
+    var bigXML = topHeader
+    println(topHeader)  
+    // Iterate on the map
+    for(pep <- pepsMap) {
      // pep._1 = key = PaPxxxxxxxx
      var featureList  = List[Feature]()
      var ftdbReflistSum = List[DbXref]()
@@ -49,7 +49,7 @@ class PeptideAtlasPhosphoNXParser extends App {
      featuresPos.foreach { pos =>
        val ftdbReflist = PeptideAtlasUtils.getDBRefForFeature(pep._2, pos, sampleIdMap)
        val modRes = PeptideAtlasUtils.getModresForFeature(pep._2, pos) // could be quicker if we build first a map (pos,modres) at peptide level
-       val feature= new Feature(_position=pos, _description=modRes, _dbrefs=ftdbReflist)
+       val feature= new Feature(_position=pos, _description=modRes, _pepid=pep._1, _dbrefs=ftdbReflist)
        ftdbReflistSum ++= ftdbReflist
        featureList = feature :: featureList
       }
@@ -58,12 +58,16 @@ class PeptideAtlasPhosphoNXParser extends App {
        }
      val sequence = pep._2(0).split("\\s+")(0)
      val peptide= new Peptide(_sequence=sequence, id=pep._1, _dbrefs=pepdbReflist.distinct, _features=featureList)
-     println(peptide.toXML)
-     //val xml = printer.format(peptide.toXML)
-     //printer.format(xml)
-     //println(xml)
+     //println(peptide.toXML)
+     val xmlpep = pprinter.format(peptide.toXML)
+     //bigXML += xmlpep
+     //println(xmlpep)
      }
-  println(pepsMap.size + " peptides successfully parsed")
+    bigXML += "</entries>" // closed
+    println("</entries>")
+    
+  //println(pepsMap.size + " peptides successfully parsed")
+  this.pep_count = pepsMap.size
   }
   
 }
