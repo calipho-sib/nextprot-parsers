f93c3364e9ab89bfee17219cb59c679f1b947985
agateau <alain.gateau@gmail.com>
2015-12-16 19:00:06 +0100

    progress

--- peptide-atlas/src/main/scala/org/nextprot/parser/peptide/atlas/PeptideAtlasPhosphoNXParser.scala
+++ peptide-atlas/src/main/scala/org/nextprot/parser/peptide/atlas/PeptideAtlasPhosphoNXParser.scala
@@ -1,6 +1,5 @@
 package org.nextprot.parser.peptide.atlas
 
-import java.io.File
 import org.nextprot.parser.core.NXParser
 import scala.io.Source
 import org.nextprot.parser.peptide.atlas.datamodel.Peptide
@@ -9,26 +8,21 @@ import org.nextprot.parser.peptide.atlas.datamodel.Feature
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.HashMap
 
-class PeptideAtlasPhosphoNXParser extends App {
+class PeptideAtlasPhosphoNXParser extends NXParser{
 
   var pInfo = ""
   var pep_count :Int = 0
   def parsingInfo: String = pInfo
   
-  //val pepsMap: HashMap[String, ArrayBuffer[String]] = new HashMap();   
-  //val pepsMap: HashMap[String, ArrayBuffer[String]]   
-  
-  def parse(filename: String) = {
-  val pprinter=new scala.xml.PrettyPrinter(240, 2)
-  val topHeader = "<entries version=\"201509\" datarelease=\"PeptideAtlas human phosphoproteome\" datasource=\"PeptideAtlas human phosphoproteome\">"
+  def parse(filename: String) : List[Peptide] = {
   // First parse the tsv file of sampleId-MDATA-pmid association and guild a map with sampleIds as keys
-  //val sampleIdMap: HashMap[String, String] = PeptideAtlasUtils.getMetadataMap1("/home/agateau/workspace/nextprot-parsers/peptide-atlas/src/test/resources/org/nextprot/parser/peptide/atlas/Metadata_phosphosetPA.tsv"); 
+  //val sampleIdMap: HashMap[String, String] = PeptideAtlasUtils.getMetadataMap1("src/test/resources/org/nextprot/parser/peptide/atlas/Metadata_phosphosetPA.tsv") 
   val sampleIdMap: HashMap[String, String] = PeptideAtlasUtils.getMetadataMap("/share/sib/common/Calipho/np/metadata_files/metadata.txt"); 
 
   // Now parse the data file and build a map with PaPids as key
    val pepsMap: HashMap[String, ArrayBuffer[String]] = new HashMap();    
    val src = Source.fromFile(filename)  
-  //pepsMap = new HashMap()
+
     for(line <- src.getLines().drop(1)) {
       val key = line.substring(0,11)
       val pepRows = pepsMap.getOrElse(key, new ArrayBuffer());
@@ -37,37 +31,29 @@ class PeptideAtlasPhosphoNXParser extends App {
     }
    src.close() 
    
-    var bigXML = topHeader
-    println(topHeader)  
     // Iterate on the map
-    for(pep <- pepsMap) {
+    val allPeps = pepsMap.map(pep =>  {
      // pep._1 = key = PaPxxxxxxxx
-     var featureList  = List[Feature]()
      var ftdbReflistSum = List[DbXref]()
      var pepdbReflist = List[DbXref]()
      val featuresPos: List[Int] = PeptideAtlasUtils.getDistinctModPos(pep._2)
-     featuresPos.foreach { pos =>
+     val featureList : List[Feature] = featuresPos.map ({ pos =>
        val ftdbReflist = PeptideAtlasUtils.getDBRefForFeature(pep._2, pos, sampleIdMap)
        val modRes = PeptideAtlasUtils.getModresForFeature(pep._2, pos) // could be quicker if we build first a map (pos,modres) at peptide level
-       val feature= new Feature(_position=pos, _description=modRes, _pepid=pep._1, _dbrefs=ftdbReflist)
        ftdbReflistSum ++= ftdbReflist
-       featureList = feature :: featureList
-      }
+       new Feature(_position=pos, _description=modRes, _pepid=pep._1, _dbrefs=ftdbReflist)
+      }).toList;
+     
      ftdbReflistSum.foreach { ftdbRef =>
        pepdbReflist = new DbXref(_mData=ftdbRef._mData, _pmid=ftdbRef._pmid, _quality=null ) :: pepdbReflist
        }
      val sequence = pep._2(0).split("\\s+")(0)
-     val peptide= new Peptide(_sequence=sequence, id=pep._1, _dbrefs=pepdbReflist.distinct, _features=featureList)
-     //println(peptide.toXML)
-     val xmlpep = pprinter.format(peptide.toXML)
-     //bigXML += xmlpep
-     //println(xmlpep)
-     }
-    bigXML += "</entries>" // closed
-    println("</entries>")
-    
-  //println(pepsMap.size + " peptides successfully parsed")
-  this.pep_count = pepsMap.size
+     
+     new Peptide(_sequence=sequence, id=pep._1, _dbrefs=pepdbReflist.distinct, _features=featureList)
+    }).toList;
+   
+   this.pep_count = pepsMap.size
+   return allPeps
   }
   
 }
