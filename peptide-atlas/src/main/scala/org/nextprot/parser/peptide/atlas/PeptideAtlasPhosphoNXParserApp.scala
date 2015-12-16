3d83a78005066cc6c5f73b3ca2820afb3798eeed
agateau <alain.gateau@gmail.com>
2015-12-16 18:56:15 +0100

    progress

--- peptide-atlas/src/main/scala/org/nextprot/parser/peptide/atlas/PeptideAtlasPhosphoNXParserApp.scala
+++ peptide-atlas/src/main/scala/org/nextprot/parser/peptide/atlas/PeptideAtlasPhosphoNXParserApp.scala
@@ -2,10 +2,10 @@ package org.nextprot.parser.peptide.atlas
 
 import org.nextprot.parser.core.NXParserAppBase
 
-object UniprotENSGNXParserApp extends NXParserAppBase {
+object PeptideAtlasPhosphoNXParserApp extends NXParserAppBase {
   
   System.setProperty("parser.impl", "org.nextprot.parser.peptide.atlas.PeptideAtlasPhosphoNXParser")
-  System.setProperty("reducer.impl", "org.nextprot.parser.peptide.atlas.NXStringFileReducer")
+  System.setProperty("reducer.impl", "org.nextprot.parser.peptide.atlas.NXPeptidePrintReducer")
 
   initialize
 }
