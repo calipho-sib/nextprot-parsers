4021df6701bd093e4356267641969dc651df5921
agateau <alain.gateau@gmail.com>
2015-12-17 12:02:13 +0100

    finalized

--- peptide-atlas/src/main/scala/org/nextprot/parser/peptide/atlas/NXPeptidePrintReducer.scala
+++ peptide-atlas/src/main/scala/org/nextprot/parser/peptide/atlas/NXPeptidePrintReducer.scala
@@ -8,7 +8,6 @@ import org.nextprot.parser.peptide.atlas.datamodel.Peptide
 class NXPeptidePrintReducer extends NXPrettyReducer {
   val dataversion = System.getProperty("version")
   val topHeader = "<entries version=\"" + dataversion + "\" datarelease=\"PeptideAtlas human phosphoproteome\" datasource=\"PeptideAtlas human phosphoproteome\">"
-  //val topHeader = "<entries version=\"201509\" datarelease=\"PeptideAtlas human phosphoproteome\" datasource=\"PeptideAtlas human phosphoproteome\">"
   val fw = new FileWriter(System.getProperty("output.file"), false)
 
   def reduce(objects: Any) = {
