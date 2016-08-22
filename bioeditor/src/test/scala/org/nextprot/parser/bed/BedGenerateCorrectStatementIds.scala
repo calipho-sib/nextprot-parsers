345664c2ef4ceb3fdfafc892a72a83c62466979a
Daniel <ddtxra@gmail.com>
2016-08-22 17:11:11 +0200

    adding a test

---
+++ bioeditor/src/test/scala/org/nextprot/parser/bed/BedGenerateCorrectStatementIds.scala
@@ -0,0 +1,25 @@
+package org.nextprot.parser.bed
+
+import org.nextprot.commons.statements.StatementField
+import org.nextprot.parsers.bed.converter.BedServiceStatementConverter
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+
+class BedGenerateCorrectStatementIds extends FlatSpec with Matchers {
+
+  BedServiceStatementConverter.addProxyDir("/Users/dteixeira/Documents/bed/");
+  val msh2Statements = BedServiceStatementConverter.convert("msh2");
+  val msh6Statements = BedServiceStatementConverter.convert("msh6");
+
+  it should "not conflict statement ids" in {
+
+    val st2 = msh2Statements.map(_.getValue(StatementField.STATEMENT_ID)).toList;
+    val st6 = msh6Statements.map(_.getValue(StatementField.STATEMENT_ID)).toList;
+    
+    assert(st2.intersect(st6).isEmpty);
+    
+  }
+
+  
+
+}
