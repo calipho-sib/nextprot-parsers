c18d5e6951d2e378905506bd8da16ab4e9d09f39
Alain Gateau <agateau@mac-058.local>
2017-01-13 15:30:36 +0100

    manage dbref to only dataset without pmid

--- peptide-atlas/src/main/scala/org/nextprot/parser/peptide/atlas/datamodel/DbXref.scala
+++ peptide-atlas/src/main/scala/org/nextprot/parser/peptide/atlas/datamodel/DbXref.scala
@@ -3,9 +3,10 @@ package org.nextprot.parser.peptide.atlas.datamodel
 case class DbXref(val _mData: String, val _quality : String, val _pmid : String) {
   // This is a case class in order to implement the 'distinct' method for list of instances
   // dbrefs at the peptide level are created with _quality=null
+  // _pmid may be a MDATA id instead in one case
   
   def toXML = 
-    <dbReference type={if(_pmid != null) "PubMed" else null} id={if(_pmid != null) {_pmid}  else null} quality={if(_quality != null) {_quality} else null}>
+    <dbReference type={if(_pmid==null) "" else if(_pmid.startsWith("MDATA")) "neXtProtSubmission" else "PubMed"} id={if(_pmid != null) {_pmid}  else null} quality={if(_quality != null) {_quality} else null}>
 			<document id={_mData}> </document>
 		</dbReference>
       
