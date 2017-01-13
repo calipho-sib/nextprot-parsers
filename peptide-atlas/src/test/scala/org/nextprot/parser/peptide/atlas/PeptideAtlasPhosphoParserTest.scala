b2bd243dec629d1ca701aad1d1895277b88bb7e6
Alain Gateau <agateau@mac-058.local>
2017-01-13 15:30:01 +0100

    minor change

--- peptide-atlas/src/test/scala/org/nextprot/parser/peptide/atlas/PeptideAtlasPhosphoParserTest.scala
+++ peptide-atlas/src/test/scala/org/nextprot/parser/peptide/atlas/PeptideAtlasPhosphoParserTest.scala
@@ -9,9 +9,9 @@ import org.scalatest.Matchers
 class PeptideAtlasPhosphoParserTest extends FlatSpec with Matchers {
 
     val dbref1 = new DbXref(_mData="MDATA_0066", _pmid="24400987", _quality=null)
-    val dbref2 = new DbXref(_mData="MDATA_0070", _pmid=null, _quality=null)
+    val dbref2 = new DbXref(_mData="MDATA_0071", _pmid="MDATA_0071_2010", _quality=null)
     val dbref3 = new DbXref(_mData="MDATA_0066", _pmid="24400987", _quality="GOLD")
-    val dbref4 = new DbXref(_mData="MDATA_0070", _pmid=null, _quality="SILVER")
+    val dbref4 = new DbXref(_mData="MDATA_0071", _pmid="MDATA_0071_2010", _quality="SILVER")
     val dbref5 = new DbXref(_mData="MDATA_0101", _pmid="18272233", _quality="SILVER")
     val feature1 = new Feature(_position=7, _description="phosphoserine", _pepid="PAp00000083", _dbrefs=List(dbref3,dbref4))
     val feature2 = new Feature(_position=20, _description="phosphoserine", _pepid="PAp00000083", _dbrefs=List(dbref4))
