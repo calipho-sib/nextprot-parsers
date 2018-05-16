9a9da26472936285096df520f14e7c32e916f993
Alain-Gateau <alain.gateau@gmail.com>
2018-05-16 17:14:49 +0200

    new evidence code for tissue specificity and expression info

--- src/main/scala/org/nextprot/parser/core/constants/EvidenceCode.scala
+++ src/main/scala/org/nextprot/parser/core/constants/EvidenceCode.scala
@@ -11,8 +11,9 @@ object EvidenceCode extends Enumeration {
 	implicit def valueToEvidenceCodeVal(x: Value) = x.asInstanceOf[Val] 
 
 	val Antibodymapping = Val("ECO:0001054","heterologous protein expression evidence") 
-	val ImmunoLocalization = Val("ECO:0001055","immunolocalization evidence") 
-	val ImmunocytoChemistry = Val("ECO:0001053","immunocytochemistry evidence") 
+	val ImmunoLocalization = Val("ECO:0000087","immunolocalization evidence") 
+	val ImmunoHistochemistry = Val("ECO:0001055","immunohistochemistry evidence") 
+	val ImmunoCytoChemistry = Val("ECO:0001053","immunocytochemistry evidence") 
 	val RnaSeq = Val("ECO:0000295","RNA-seq evidence") 
 } 
 
