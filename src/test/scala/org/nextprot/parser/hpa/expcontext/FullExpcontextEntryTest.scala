f4cb76a8018ba91431d20a003eec333b242edf7b
Valentine Rech de Laval <Valentine.RechdeLaval@isb-sib.ch>
2019-10-31 15:05:06 +0100

    Fix tests following HPA parsing modifications

--- src/test/scala/org/nextprot/parser/hpa/expcontext/FullExpcontextEntryTest.scala
+++ src/test/scala/org/nextprot/parser/hpa/expcontext/FullExpcontextEntryTest.scala
@@ -3,15 +3,19 @@ package org.nextprot.parser.hpa.expcontext
 import org.scalatest._
 import org.nextprot.parser.core.NXParser
 import java.io.File
+
 import org.nextprot.parser.core.exception.NXException
 import org.nextprot.parser.hpa.subcell.cases._
 import org.nextprot.parser.hpa.HPAUtils
+import org.nextprot.parser.hpa.HPAUtils.dataNotBloodOrgan
 import org.nextprot.parser.hpa.HPAQuality
 import org.nextprot.parser.hpa.commons.constants.HPAValidationValue._
 import org.nextprot.parser.hpa.commons.constants.HPAValidationValue
+
 import scala.util.matching.Regex
-import scala.xml.PrettyPrinter
+import scala.xml.{Elem, PrettyPrinter}
 import java.io.FileWriter
+
 import org.nextprot.parser.core.datamodel.TemplateModel
 
 class FullExpcontextEntryTest extends HPAExpcontextTestBase {
@@ -61,7 +65,12 @@ class FullExpcontextEntryTest extends HPAExpcontextTestBase {
 
   "The HPAExpcontextNXParser " should " contains 4 comments about inconsistencies in TEST hpa-caloha mapping file" in {
 
-    // old version of mapping file causing problems
+    // NextProt_tissues.from-db.with2warnings.txt should contains two terms should be mapped to two different terms
+    // Currently, the mapped terms to two different terms are:
+    // Myelencephalon|Myelencephalon|TS-2365
+    // Myelencephalon|Medulla oblongata|TS-0607
+    // Kidney proximal tubule|Renal proximal tubule|TS-0509
+    // Kidney proximal tubule|Renal proximal convoluted tubule|TS-2198
     val hpaParser = new HPAExpcontextNXParser();
     val fname = "src/test/resources/hpa/expression/ENSG00000272047.xml";
     val result = hpaParser.parse(fname);
@@ -69,10 +78,10 @@ class FullExpcontextEntryTest extends HPAExpcontextTestBase {
     result.dataset.foreach(accumulator.accumulateCalohaMapping(_));
     val model = accumulator.getTemplateModel();
 
-    val pp = new PrettyPrinter(1000,4)
-    val str = pp.format(model.toXML).substring(0,800)
-    //println(str)
-    
+//    val pp = new PrettyPrinter(1000,4)
+//    val str = pp.format(model.toXML);
+//    println("model: " + str)
+
     assert(
       model.toXML.child.
         filter(_.isInstanceOf[scala.xml.Comment]).
@@ -85,8 +94,11 @@ class FullExpcontextEntryTest extends HPAExpcontextTestBase {
   // see also properties set in HPAExpcontextTestBase for "hpa.tissue.mapping.file" = hpa - caloha mappping file
   "The HPAExpcontextNXParser " should " contain 1 comment about hpa terms not found in TEST hpa-caloha mapping file" in {
 
+    // NextProt_tissues.from-db.with2warnings.txt is a copy of NextProt_tissues.from-db.txt
+    // with one mapped term (which is in the xml file) is removed.
+    // Currently, 'Olfactory region' is removed
     val hpaParser = new HPAExpcontextNXParser();
-    val fname = "src/test/resources/hpa/expression/ENSG00000272048.xml";
+    val fname = "src/test/resources/hpa/expression/ENSG00000272333.xml";
     val result = hpaParser.parse(fname);
     //println(wrapper.toXML.toString)
 
@@ -103,14 +115,16 @@ class FullExpcontextEntryTest extends HPAExpcontextTestBase {
   }
 
 
-  "The HPAExpcontextNXParser " should " contains find 37 tissues in RNA expression and produce corresponding ECs" in {
+  "The HPAExpcontextNXParser " should " contains find 54 consensus tissues and 20 blood cells in RNA expression and produce corresponding ECs" in {
 
     val hpaParser = new HPAExpcontextNXParser();
     val fname = "src/test/resources/hpa/expression/ENSG00000272333.xml";
 
     val xmlin = scala.xml.XML.loadFile(fname)
-    val expectedCount = (xmlin \\ "rnaExpression" \\ "tissue").size // no <tissueCell> in RNA expression
-    println("INPUT - count of tissue in RNA expression section: " + expectedCount)  // 37 tissues
+    val (consensusExpectedCount: Int, bloodExpectedCount: Int, expectedCount: Int) = getExpectedCount(xmlin)
+
+    println("INPUT - count of tissue in RNA expression section: " + consensusExpectedCount + " consensus tissues and " +
+      bloodExpectedCount + " blood cells")  // 54 consensus tissues and 20 blood cells
     
     val result = hpaParser.parse(fname);
     val accumulator = new ExpcontextAccumulator(HPAExpcontextConfig.readTissueMapFileFromFile(new File("src/test/resources/NextProt_tissues.from-db.txt")));
@@ -120,7 +134,7 @@ class FullExpcontextEntryTest extends HPAExpcontextTestBase {
     
     //println("----- RAW ------")
     //val prettyPrinter = new PrettyPrinter(1000, 4)
-    //println(prettyPrinter.format(xmlount))
+    //println(prettyPrinter.format(xmlout))
     //println("----- x x x ------")
     
     val ecCount = (xmlout \ "com.genebio.nextprot.dataloader.context.ExperimentalContextWrapper").size
@@ -142,6 +156,13 @@ class FullExpcontextEntryTest extends HPAExpcontextTestBase {
     
   }
 
+  private def getExpectedCount(xmlin: Elem) = {
+    val consensusExpectedCount = ((xmlin \ "rnaExpression" filter { _ \\ "@assayType" exists (_.text == "consensusTissue") })
+      \ "data" filter dataNotBloodOrgan("tissue")).size;
+    val bloodExpectedCount = ((xmlin \ "rnaExpression" filter { _ \\ "@assayType" exists (_.text == "blood") }) \ "data").size;
+    val expectedCount = consensusExpectedCount + bloodExpectedCount;
+    (consensusExpectedCount, bloodExpectedCount, expectedCount)
+  }
 
   "The HPAExpcontextNXParser " should " find tissues in IHC and RNA expression and produce corresponding EC synonyms" in {
 
@@ -149,8 +170,8 @@ class FullExpcontextEntryTest extends HPAExpcontextTestBase {
     val fname = "src/test/resources/hpa/expression/ENSG00000000003.xml";
 
     val xmlin = scala.xml.XML.loadFile(fname)
-    val expectedRNACount = (xmlin \ "rnaExpression" \\ "tissue").size        // we have    no <tissueCell>, only <tissue>
-    val expectedIHCCount = (xmlin \ "tissueExpression" \\ "tissueCell").size -6 // (minus duplicates for endometrium, soft tissues and stomach)we may have N <tissueCell> for 1 <tissue>
+    val (consensusExpectedCount: Int, bloodExpectedCount: Int, expectedRNACount: Int) = getExpectedCount(xmlin)
+    val expectedIHCCount = (xmlin \ "tissueExpression" \\ "tissueCell").size
     val expectedCount = expectedRNACount + expectedIHCCount
     println("INPUT - count of tissue in RNA expression section: " + expectedRNACount)  // 37 tissues
     println("INPUT - count of tissue in IHC expression section: " + expectedIHCCount)  // 80-6 = 74 tissues
@@ -173,7 +194,9 @@ class FullExpcontextEntryTest extends HPAExpcontextTestBase {
     val synoList = (xmlout.child \ "wrappedBean" \ "contextSynonyms" \\ "synonymName").toList  // we may have N synonyms in 1 ec
     println("OUTPUT - synoCount:" + synoList.size)
     assert(synoList.size == expectedCount)
+
     synoList.foreach(n => assert(n.text.contains("eco->") && n.text.contains("tissue->")))
+
     val methods = synoList.groupBy(n => methodGroup(n.text)).map(el => (el._1, el._2.length)).toMap
     println("Synonyms by method:" + methods)
     assert(methods("IHC") == expectedIHCCount)
